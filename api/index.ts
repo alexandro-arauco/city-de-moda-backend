0ee10e653a10b106282606f8ee5ca43d8f9c08ef
Alexandro Arauco Sanchez <xander.as90@gmail.com>
2025-03-20 19:21:26 +0100

    Add 5-minute timeout for requests in API middleware

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,10 @@
 import app from "../src/server";
 
+app.use((req, res, next) => {
+  req.setTimeout(5 * 60 * 1000); // 5-minute timeout
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("Hello from Vercel!");
 });
